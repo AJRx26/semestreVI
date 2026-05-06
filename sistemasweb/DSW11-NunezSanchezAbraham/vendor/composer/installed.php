8d9cfcd64ab7fbc13e774d352b3be41c888c9386
Skull_AJR <abraham.nunez.sanchez@gmail.com>
2026-05-05 18:16:15 -0600

    version12.0

--- sistemasweb/DSW11-NunezSanchezAbraham/vendor/composer/installed.php
+++ sistemasweb/DSW11-NunezSanchezAbraham/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '52914e41111f8ff793a16bcae5fa4d7ab0ce8574',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '52914e41111f8ff793a16bcae5fa4d7ab0ce8574',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
