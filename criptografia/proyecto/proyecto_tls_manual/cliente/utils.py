941ffb3ea028217ec6200f465900ddd12c589263
Skull_AJR <abraham.nunez.sanchez@gmail.com>
2026-06-16 10:03:43 -0600

    version17.4

--- criptografia/proyecto/proyecto_tls_manual/cliente/utils.py
+++ criptografia/proyecto/proyecto_tls_manual/cliente/utils.py
@@ -1,6 +1,8 @@
 """
 utils.py.
-Módulo de utileria para manejo de mensajes/cifrado/descifrado en el repositorio.
+Aquí se encuentran las funciones compartidas entre servidor y cliente.
+Módulo de utileria para envío/recepción de mensajes,
+para cifrado/descifrado y lectura de archivos en el repositorio.
 """
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
@@ -24,11 +26,19 @@ def _recibir_exacto(sock, n):
 
 
 def enviar_bytes(sock, data: bytes):
+    """
+    Función para longitud fija, 
+    se envían 4 bytes que indican que tan grande es el mensaje real.
+    """
     longitud = len(data).to_bytes(4, 'big')
     sock.sendall(longitud + data)
 
 
 def recibir_bytes(sock) -> bytes:
+    """
+    Función para longitud fija, 
+    se reciben 4 bytes que indican que tan grande es el mensaje real.
+    """
     longitud = int.from_bytes(_recibir_exacto(sock, 4), 'big')
     return _recibir_exacto(sock, longitud)
 
@@ -41,10 +51,19 @@ def leer_mensaje(sock) -> bytes:
     return recibir_bytes(sock)
 
 
+# Fase 2: Funciones para el Handshake (ECDHE)
+
 def generar_llave_efimera():
+    """
+    Función para crear una llave privada efímera usando curvas elípticas.
+    """
     return ec.generate_private_key(ec.SECP384R1())
 
 def derivar_llaves(shared_key):
+    """
+    Se usa el algoritmo HKDF para transformar el secreto compartido.
+    Genera dos llaves cruzadas simétricas de 32 bytes. Una para envíar otra para recibir.
+    """
     derived = HKDF(
         algorithm=hashes.SHA256(),
         length=64, salt=None,
@@ -54,16 +73,19 @@ def derivar_llaves(shared_key):
     return derived[:32], derived[32:]
 
 
+# FASE 4: Operaciones de repositorio.
+
 def crear_generador_lectura(path_archivo, tam_lectura=4096):
     """
-    Lee archivos en pedazos para no saturar la RAM.
+    Lee archivos en pedazos (4096 bytes) para no saturar la RAM.
     """
     with open(path_archivo, 'rb') as archivo:
         while True:
             contenido = archivo.read(tam_lectura)
             if not contenido:
                 break
-            yield contenido
+            yield contenido # yield -> hacer una pausa después de leer un pezado
+            # se entrega para cifrar y solo se lee el siguiente pedazo cuando se solicita
 
 
 def regresar_bytes(path_archivo):
@@ -76,17 +98,21 @@ def regresar_bytes(path_archivo):
 def cifrar(llave, datos, timestamp_entero):
     """
     Cifra un segmento con AES-GCM incluyendo datos autenticados adicionales (AAD).
-    timestamp_entero: El tiempo actual como int (ej. int(time.time()))
+    Los pasos son:
+    1. Convierte el tiempo a 8 bytes exactos para el AAD.
+    2. Genera un IV de 12 bytes único.
+    3. Cifra con AES-GCM.
+    Return: [IV] + [Timestamp] + [TAG] + [Cifrado]
     """
     # Aseguramos que el timestamp siempre ocupe 8 bytes exactos
     timestamp_bytes = timestamp_entero.to_bytes(8, 'big')
                
     iv = os.urandom(12) # IV estándar de 12 bytes para GCM
-    encryptor = Cipher(algorithms.AES(llave), 
+    encryptor = Cipher(algorithms.AES(llave), # llave_sesion_enviar
                        modes.GCM(iv),
                        backend=default_backend()).encryptor()
 
-    # Autenticamos el contexto (IV y Timestamp) como AAD
+    # Autenticamos datos (IV y Timestamp) como AAD
     associated_data = iv + timestamp_bytes 
     encryptor.authenticate_additional_data(associated_data)                
 
@@ -101,13 +127,18 @@ def cifrar(llave, datos, timestamp_entero):
 def descifrar(llave, paquete):
     """
     Descifra el paquete con AES-GCM [iv + timestamp_bytes + tag + cifrado].
+    Los pasos son:
+    1. Extrae cada componente con rebanadas precisas.
+    2. Configura modo GCM usando el IV y el TAG.
+    3. Verifica timestamp no sea mayor a 7200 seg respecto al reloj local
+    4. Si TAG no conincide en finalize() lanza excepción.
     """
-    iv = paquete[:12]
-    timestamp_bytes = paquete[12:20]
-    tag = paquete[20:36]
-    cifrado = paquete[36:]
+    iv = paquete[:12] # 12 bytes
+    timestamp_bytes = paquete[12:20] # 8 bytes
+    tag = paquete[20:36] # 16 bytes
+    cifrado = paquete[36:] # contenido restante
 
-    decryptor = Cipher(algorithms.AES(llave), 
+    decryptor = Cipher(algorithms.AES(llave), # llave_sesion_recibir
         modes.GCM(iv, tag),
         backend=default_backend()).decryptor()
 
@@ -115,15 +146,17 @@ def descifrar(llave, paquete):
     decryptor.authenticate_additional_data(associated_data)
 
     try:
-        datos = decryptor.update(cifrado)
-        datos += decryptor.finalize() # Valida integridad y AAD
-    except InvalidTag:
-        raise Exception("Error de integridad: el segmento ha sido manipulado.")
+        datos = decryptor.update(cifrado) # descifra los datos
+        datos += decryptor.finalize() # Recalcula el tag con los datos recibidos y lo compara
+    except InvalidTag: # si el IV o el Timestamp fueron alterados lanza excepción
+        raise Exception("[ERROR] Error de integridad: el segmento ha sido manipulado.")
 
-    timestamp_del_paquete = int.from_bytes(timestamp_bytes, 'big')
-    tiempo_ahora = int(time.time())
+    # Toma los 8 bytes extraídos y los transforma en número entero para python
+    timestamp_del_paquete = int.from_bytes(timestamp_bytes, 'big') # big --> significativo al menos
+    tiempo_ahora = int(time.time()) # Pide al SO la hora actual en formato Unix
 
-    if abs(tiempo_ahora - timestamp_del_paquete) > 7200:
+    # resta -> diferencia en segundos entre: momento en que se generó paquete y momento en que servidor procesa
+    if abs(tiempo_ahora - timestamp_del_paquete) > 7200: # abs -> convertir número negativo en positivo
         raise Exception("El segmento ha caducado (más de 2 horas)")
     
     return datos
